13d78c50c59d80f2287eb0ddc61a96d10306056a
hakobera <hakobera@gmail.com>
2011-12-11 16:05:48 +0900

    Add files for Heroku

--- app.js
+++ app.js
@@ -112,5 +112,5 @@ app.post('/submit', function(req, res, next) {
   }
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
